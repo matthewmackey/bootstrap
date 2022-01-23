55869fbe8814e7041467aa37c1756aa0823cf93a
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2022-01-22 21:35:56 -0500

    Now cloning .personal repos from Github

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
+
+# gpg
 GPG_KEY_TYPE=RSA
 GPG_KEY_LENGTH=4096
-
 GPG_NAME="Matthew Machaj"
 GPG_EMAIL="matthew.mackey.dev@gmail.com"
 GPG_COMMENT="<COMPUTER_NAME>"
 
-SSH_KEY_TYPE=rsa
-SSH_KEY_LENGTH=4096
-DEFAULT_SSH_KEY_FILE=~/.ssh/id_rsa2
-
+# pass
 PASS_DEFAULT_SSH_PASSPHRASE_PATH="ssh/default"
 
+# ssh
+SSH_KEY_TYPE=rsa
+SSH_KEY_LENGTH=4096
+DEFAULT_SSH_KEY_FILE=~/.ssh/id_rsa
 
+# github
+GITHUB_USER=matthewmackey
+GITHUB_NAME="Matthew Machaj"
+GITHUB_EMAIL="21043873+matthewmackey@users.noreply.github.com"
+GITHUB_BASE_REPO=https://github.com/$GITHUB_USER
+
+# 'personal' bootstrap system
+PERSONAL_BASE_DIR=~/.personal
+PERSONAL_BIN_DIR=$PERSONAL_BASE_DIR/bin
+PERSONAL_CONFIG_MGMT_DIR=$PERSONAL_BASE_DIR/config-mgmt
+PERSONAL_DOTFILES_DIR=$PERSONAL_BASE_DIR/dotfiles
+
+PERSONAL_BIN_REPO=$GITHUB_BASE_REPO/bin.git
+PERSONAL_DOTFILES_REPO=$GITHUB_BASE_REPO/dotfiles.git
+PERSONAL_CONFIG_MGMT_REPO=$GITHUB_BASE_REPO/config-mgmt.git
+
+#-------------------------------------------------------------------------------
+# Helper methods
+#-------------------------------------------------------------------------------
 print_step() {
   printf "\n"
   printf "#-------------------------------------------------------------------------------\n"
@@ -21,26 +45,26 @@ print_step() {
   printf "#-------------------------------------------------------------------------------\n"
 }
 
-
-install_packages() {
-  sudo apt-get update
-  sudo apt-get install -y git pass
+print_msg() {
+  printf "\n $1\n"
 }
 
-generate_gpg_key() {
-  cat <<EOF | gpg --gen-key --batch
-Key-Type: $GPG_KEY_TYPE
-Key-Length: $GPG_KEY_LENGTH
-Subkey-Type: $GPG_KEY_TYPE
-Subkey-Length: $GPG_KEY_LENGTH
-Name-Real: $GPG_NAME
-Name-Email: $GPG_EMAIL
-Name-Comment: $GPG_COMMENT
-Expire-Date: 0
-%commit
-EOF
+skip_if_exists() {
+  local _file_to_test_if_exists=$1
+  local _skip_msg=$2
+
+  if [ -e $_file_to_test_if_exists ]; then
+    printf "$_skip_msg\n"
+    printf "[$_file_to_test_if_exists] already exists...skipping step\n"
+    return 1
+  else
+    return 0
+  fi
 }
 
+#-------------------------------------------------------------------------------
+# Steps:
+#-------------------------------------------------------------------------------
 gather_computer_id() {
   print_step "Gather Computer ID"
 
@@ -55,14 +79,44 @@ gather_computer_name() {
   read -p "$_prompt" COMPUTER_NAME
 }
 
+install_packages() {
+  print_step "Install required packages"
+  sudo apt-get update
+  sudo apt-get install -y gpg git pass
+}
+
+generate_gpg_key() {
+  print_step "Generate GPG key for [$GPG_EMAIL]"
+
+  if gpg --list-keys $GPG_EMAIL >/dev/null; then
+    printf "GPG key already exists for [$GPG_EMAIL]...skipping step\n"
+    return 0
+  fi
+
+  cat <<EOF | gpg --gen-key --batch
+Key-Type: $GPG_KEY_TYPE
+Key-Length: $GPG_KEY_LENGTH
+Subkey-Type: $GPG_KEY_TYPE
+Subkey-Length: $GPG_KEY_LENGTH
+Name-Real: $GPG_NAME
+Name-Email: $GPG_EMAIL
+Name-Comment: $GPG_COMMENT
+Expire-Date: 0
+%commit
+EOF
+}
 
 initialize_pass_repo() {
   print_step "Initialize 'pass' repository"
+
+  _skip_msg="'pass' repository has already been initialized"
+  skip_if_exists ~/.password-store "$_skip_msg" || return 0
+
   pass init "$GPG_EMAIL"
   pass git init
 }
 
-gather_ssh_key_passphrase() {
+gather_and_store_ssh_key_passphrase() {
   print_step "Gather passphrase for default SSH key & insert into 'pass' repo"
 
   printf "The following passphrase prompt will be for the your default SSH key ($DEFAULT_SSH_KEY_FILE).\n"
@@ -75,6 +129,9 @@ gather_ssh_key_passphrase() {
 generate_ssh_key() {
   print_step "Generate default SSH key"
 
+  _skip_msg="Default SSH key already exists"
+  skip_if_exists $DEFAULT_SSH_KEY_FILE "$_skip_msg" || return 0
+
   printf "You will now be prompted for your GPG passphrase to retrieve the SSH passphrase that was just added to your 'pass' repo.\n"
   printf "The passphrase will then be used with'ssh-keygen' to create your default SSH key.\n\n"
   read -p "Hit ENTER to continue "
@@ -84,24 +141,49 @@ generate_ssh_key() {
     -N "$(pass show "$PASS_DEFAULT_SSH_PASSPHRASE_PATH")"
 }
 
-
-main() {
-
-  COMPUTER_NAME="Lenovo laptop"
-  gather_computer_name
-
-  # install_packages
-  # generate_gpg_key
+clone_dot_personal_repos() {
+  clone_if_not_exist $PERSONAL_BIN_DIR $PERSONAL_BIN_REPO
+  clone_if_not_exist $PERSONAL_DOTFILES_DIR $PERSONAL_DOTFILES_REPO
+  clone_if_not_exist $PERSONAL_CONFIG_MGMT_DIR $PERSONAL_CONFIG_MGMT_REPO
 }
 
-COMPUTER_NAME="Lenovo laptop"
-gather_computer_name
+clone_if_not_exist() {
+  local _clone_dir=$1
+  local _clone_url=$2
+
+  print_step "Cloning [$_clone_url]"
+
+  _skip_msg=".personal git repo [$_clone_url] already cloned"
+  skip_if_exists $_clone_dir || return 0
+
+  git clone $_clone_url $_clone_dir
+  cd $_clone_dir
+
+  print_msg "Setting local git config name [$GITHUB_NAME]"
+  git config --local user.name "$GITHUB_NAME"
+
+  print_msg "Setting local git config email [$GITHUB_EMAIL]"
+  git config --local user.email "$GITHUB_EMAIL"
+}
+
+
+#-------------------------------------------------------------------------------
+# main()
+#-------------------------------------------------------------------------------
+# install_packages
 
 COMPUTER_ID="mmlenovo"
 gather_computer_id
 
+COMPUTER_NAME="Lenovo laptop"
+gather_computer_name
+
+generate_gpg_key
+
 initialize_pass_repo
-gather_ssh_key_passphrase
 
 SSH_COMMENT="$USER-$COMPUTER_ID"
+gather_and_store_ssh_key_passphrase
 generate_ssh_key
+
+clone_dot_personal_repos
