9808b99af92e4537b0a544c17ffa9537759e6e81
Matthew Mackey <7613567-matthewmackey@users.noreply.gitlab.com>
2024-09-21 18:31:56 -0400

    Replaced manual py install w/ asdf and pipx for Ansible setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
-# TODO: how to figure out dependency on the PERSONAL_DIR variable with the other
-# dotfile-related repos
-export PERSONAL_DIR=~/.personal
 
-ANSIBLE_ROOT=$PERSONAL_DIR/config-mgmt
+#------------------------------------------------------------------------------
+# DEPENDENCIES
+#------------------------------------------------------------------------------
+ENV_COMMON_URL="https://raw.githubusercontent.com/matthewmackey/dotfiles/refs/heads/main/.config/sh/env"
+LIB_COMMON_URL="https://raw.githubusercontent.com/matthewmackey/dotfiles/refs/heads/main/lib/common.sh"
+
+# See: https://stackoverflow.com/questions/5735666/execute-bash-script-from-url
+source <(curl -s $ENV_COMMON_URL)
+source <(curl -s $LIB_COMMON_URL)
+
+
+#------------------------------------------------------------------------------
+# MAIN()
+#------------------------------------------------------------------------------
 USERNAME=mmackey
 TMP_BOOTSTRAP_DIR=/tmp/bootstrap
+
+export PERSONAL_DIR=$PERSONAL
+ANSIBLE_ROOT=$PERSONAL_DIR/config-mgmt
+
+sudo apt install -y git
 
 # Clone bootstrap repository
 git clone https://github.com/matthewmackey/bootstrap.git $TMP_BOOTSTRAP_DIR
@@ -17,22 +33,20 @@ cd $TMP_BOOTSTRAP_DIR
 # Run script to clone and setup pass, SSH key and other repos needed for bootstrapping
 ./bootstrap-pass-ssh-repos.sh
 
-# Setup ASDF so we can install Python and pip to create an Ansible virtualenv
+# Setup ASDF so we can install Python and pip to install Ansible
 ./bootstrap-asdf.sh
 
-# Run script to setup a tmp Ansible installation we can use to bootstrap a real Ansible installation
+# Install Ansible via pipx
+./bootstrap-ansible.sh
 
 
-# Run a bootstrap playbook using the tmp Ansible installation
-#
-# FYI, the `local` inventory file (possible corresponding `host_vars`) is something that is
-# not checked into VCS in the `config- mgmt` repository b/c it can vary by machine.
-/tmp/ansible_tmp_python/ansible.sh \
-  -v \
-  -i $ANSIBLE_ROOT/local \
-  -e username=$USERNAME \
-  $ANSIBLE_ROOT/play-bootstrap.yml
+## Run a bootstrap playbook using the tmp Ansible installation
+##
+## FYI, the `local` inventory file (possible corresponding `host_vars`) is something that is
+## not checked into VCS in the `config- mgmt` repository b/c it can vary by machine.
+#/tmp/ansible_tmp_python/ansible.sh \
+#  -v \
+#  -i $ANSIBLE_ROOT/local \
+#  -e username=$USERNAME \
+#  $ANSIBLE_ROOT/play-bootstrap.yml
 
-# Use the new Python installed by the bootstrap playbook to create a permanent Ansible installation
-mkvirtualenv ansible
-pip install ansible
